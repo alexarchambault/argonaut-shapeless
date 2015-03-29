b878167c06adda90ad099c84c7c400bde2acaaa9
Denis Mikhailov <mda.mymail@gmail.com>
2015-03-29 21:55:34 +0300

    Refactor AutoEncodeJsons to use CoproductContainer

--- src/main/scala/argonaut/AutoEncodeJsons.scala
+++ src/main/scala/argonaut/AutoEncodeJsons.scala
@@ -3,7 +3,24 @@ package argonaut
 import Json._
 import shapeless._, labelled.FieldType
 
-trait AutoEncodeJsons {  
+trait AutoEncodeJsons1 {
+
+  def cconsEncodeJson[K <: Symbol, H, T <: Coproduct](implicit
+                                                      key: Witness.Aux[K],
+                                                      headEncode: Lazy[EncodeJson[H]],
+                                                      tailEncode: Lazy[EncodeJson[T]],
+                                                      coproductContainer: CoproductContainer
+                                                       ): EncodeJson[FieldType[K, H] :+: T]
+
+  implicit def defaultCConsEncodeJson[K <: Symbol, H, T <: Coproduct](implicit
+                                                                      key: Witness.Aux[K],
+                                                                      headEncode: Lazy[EncodeJson[H]],
+                                                                      tailEncode: Lazy[EncodeJson[T]]
+                                                                       ): EncodeJson[FieldType[K, H] :+: T] =
+    cconsEncodeJson(key, headEncode, tailEncode, CoproductContainer.default)
+}
+
+trait AutoEncodeJsons extends AutoEncodeJsons1 {
   implicit def hnilEncodeJson[L <: HNil]: EncodeJson[L] =
     EncodeJson(_ => jEmptyObject)
 
@@ -19,13 +36,15 @@ trait AutoEncodeJsons {
   implicit val cnilEncodeJson: EncodeJson[CNil] =
     EncodeJson(_ => jEmptyObject)
 
+
   implicit def cconsEncodeJson[K <: Symbol, H, T <: Coproduct](implicit
     key: Witness.Aux[K],
     headEncode: Lazy[EncodeJson[H]],
-    tailEncode: Lazy[EncodeJson[T]]
+    tailEncode: Lazy[EncodeJson[T]],
+    coproductContainer: CoproductContainer
   ): EncodeJson[FieldType[K, H] :+: T] =
     EncodeJson {
-      case Inl(h) => Json(key.value.name -> headEncode.value.encode(h))
+      case Inl(h) => coproductContainer(key.value.name, headEncode.value.encode(h))
       case Inr(t) => tailEncode.value.encode(t)
     }
 
