b940fdd5c0ff142581905517205b5728fa6423de
Denis Mikhailov <mda.mymail@gmail.com>
2015-03-29 21:54:55 +0300

    Implement default CoproductContainer

--- src/main/scala/argonaut/CoproductContainer.scala
+++ src/main/scala/argonaut/CoproductContainer.scala
@@ -4,3 +4,16 @@ trait CoproductContainer {
   def apply(typeName: String, content: Json): Json
   def unapply(json: HCursor): Option[(String, Json)]
 }
+
+object CoproductContainer {
+  def default = new CoproductContainer {
+    override def apply(typeName: String, content: Json): Json = Json(typeName -> content)
+    override def unapply(c: HCursor): Option[(String, Json)] = {
+      c.focus.objectFields.flatMap(_.headOption).flatMap { field =>
+        c.focus.field(field).map { value =>
+          (field, value)
+        }
+      }
+    }
+  }
+}
