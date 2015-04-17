d973d2032851e7a4e22cfe13a70409889b804c5c
Denis Mikhaylov <notxcain@gmail.com>
2015-04-17 17:56:59 +0500

    Add Custom machinery skeleton

--- src/main/scala/argonaut/Shapeless.scala
+++ src/main/scala/argonaut/Shapeless.scala
@@ -1,3 +1,5 @@
 package argonaut
 
-object Shapeless extends AutoDecodeJsons with AutoEncodeJsons with DefaultJsonCoproductCodec
+object Shapeless extends AutoDecodeJsons with AutoEncodeJsons with DefaultJsonCoproductCodec {
+  object Custom extends CustomGenericDecodeJsons with CustomGenericEncodeJsons
+}
