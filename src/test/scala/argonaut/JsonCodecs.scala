f85e6ca8ec291e1ed2c1018a6331e62c124c006d
Alexandre Archambault <alexandre.archambault@gmail.com>
2015-05-12 01:55:28 +0200

    Switch to shapeless 2.2.0-RC6
    Missing: more detailed tests, orphaned derivation related things?

--- src/test/scala/argonaut/JsonCodecs.scala
+++ src/test/scala/argonaut/JsonCodecs.scala
@@ -1,5 +1,7 @@
 package argonaut
 
+import shapeless.cachedImplicit
+
 // Defined here and not in Definitions.scala because of https://issues.scala-lang.org/browse/SI-7755
 sealed trait Base
 case class BaseIS(i: Int, s: String) extends Base
@@ -11,27 +13,27 @@ case class BaseLast(c: Simple) extends Base
 object JsonCodecs {
   import Argonaut._, Shapeless._
 
-  implicit val emptyEncodeJson = EncodeJson.of[Empty.type]
-  implicit val emptyDecodeJson = DecodeJson.of[Empty.type]
+  implicit val emptyEncodeJson: EncodeJson[Empty.type] = cachedImplicit
+  implicit val emptyDecodeJson: DecodeJson[Empty.type] = cachedImplicit
 
-  implicit val emptyCCEncodeJson = EncodeJson.of[EmptyCC]
-  implicit val emptyCCDecodeJson = DecodeJson.of[EmptyCC]
+  implicit val emptyCCEncodeJson: EncodeJson[EmptyCC] = cachedImplicit
+  implicit val emptyCCDecodeJson: DecodeJson[EmptyCC] = cachedImplicit
 
-  implicit val simpleEncodeJson = EncodeJson.of[Simple]
-  implicit val simpleDecodeJson = DecodeJson.of[Simple]
+  implicit val simpleEncodeJson: EncodeJson[Simple] = cachedImplicit
+  implicit val simpleDecodeJson: DecodeJson[Simple] = cachedImplicit
 
-  implicit val composedEncodeJson = EncodeJson.of[Composed]
-  implicit val composedDecodeJson = DecodeJson.of[Composed]
+  implicit val composedEncodeJson: EncodeJson[Composed] = cachedImplicit
+  implicit val composedDecodeJson: DecodeJson[Composed] = cachedImplicit
 
-  implicit val twiceComposedEncodeJson = EncodeJson.of[TwiceComposed]
-  implicit val twiceComposedDecodeJson = DecodeJson.of[TwiceComposed]
+  implicit val twiceComposedEncodeJson: EncodeJson[TwiceComposed] = cachedImplicit
+  implicit val twiceComposedDecodeJson: DecodeJson[TwiceComposed] = cachedImplicit
 
-  implicit val composedOptListEncodeJson = EncodeJson.of[ComposedOptList]
-  implicit val composedOptListDecodeJson = DecodeJson.of[ComposedOptList]
+  implicit val composedOptListEncodeJson: EncodeJson[ComposedOptList] = cachedImplicit
+  implicit val composedOptListDecodeJson: DecodeJson[ComposedOptList] = cachedImplicit
 
-  implicit val nowThreeEncodeJson = EncodeJson.of[NowThree]
-  implicit val nowThreeDecodeJson = DecodeJson.of[NowThree]
+  implicit val nowThreeEncodeJson: EncodeJson[NowThree] = cachedImplicit
+  implicit val nowThreeDecodeJson: DecodeJson[NowThree] = cachedImplicit
 
-  implicit val baseEncodeJson = EncodeJson.of[Base]
-  implicit val baseDecodeJson = DecodeJson.of[Base]
+  implicit val baseEncodeJson: EncodeJson[Base] = cachedImplicit
+  implicit val baseDecodeJson: DecodeJson[Base] = cachedImplicit
 }
