80fd4ec1364ff080151e33ef353d0d9212171ac4
Alexandre Archambault <alexandre.archambault@gmail.com>
2015-06-22 13:24:13 +0200

    Cleaning

--- src/main/scala/argonaut/AutoDecodeJsons.scala
+++ src/main/scala/argonaut/AutoDecodeJsons.scala
@@ -7,51 +7,51 @@ trait AutoDecodeJsons {
 
   object DecodeJsonDeriver {
 
-  implicit val hnilDecodeJson: DecodeJson[HNil] =
-    DecodeJson { c =>
-      // if (c.focus.obj.exists(_.isEmpty))
-      (HNil: HNil).point[DecodeResult]
-      // else
-      //   DecodeResult.fail("HNil", c.history)
-    }
+    implicit val hnilDecodeJson: DecodeJson[HNil] =
+      DecodeJson { c =>
+        // if (c.focus.obj.exists(_.isEmpty))
+        (HNil: HNil).point[DecodeResult]
+        // else
+        //   DecodeResult.fail("HNil", c.history)
+      }
 
-  implicit def hconsDecodeJson[K <: Symbol, H, T <: HList](implicit
-    key: Witness.Aux[K],
-    headDecode: Lazy[DecodeJson[H]],
-    tailDecode: Lazy[DecodeJson[T]]
-  ): DecodeJson[FieldType[K, H] :: T] =
-    DecodeJson { c =>
-      val headJson = c --\ key.value.name
-      val head = headJson.as(headDecode.value).map(field[K](_))
-      lazy val tail = headJson.delete.as(tailDecode.value)
-      apply2(head, tail)(_ :: _)
-    }
+    implicit def hconsDecodeJson[K <: Symbol, H, T <: HList](implicit
+      key: Witness.Aux[K],
+      headDecode: Lazy[DecodeJson[H]],
+      tailDecode: Lazy[DecodeJson[T]]
+    ): DecodeJson[FieldType[K, H] :: T] =
+      DecodeJson { c =>
+        val headJson = c --\ key.value.name
+        val head = headJson.as(headDecode.value).map(field[K](_))
+        lazy val tail = headJson.delete.as(tailDecode.value)
+        apply2(head, tail)(_ :: _)
+      }
 
-  implicit val cnilDecodeJson: DecodeJson[CNil] = 
-    DecodeJson(c => DecodeResult.fail("CNil", c.history))
+    implicit val cnilDecodeJson: DecodeJson[CNil] = 
+      DecodeJson(c => DecodeResult.fail("CNil", c.history))
 
-  implicit def cconsDecodeJson[K <: Symbol, H, T <: Coproduct](implicit
-    key: Witness.Aux[K],
-    headDecode: Lazy[DecodeJson[H]],
-    tailDecode: Lazy[DecodeJson[T]]
-  ): DecodeJson[FieldType[K, H] :+: T] =
-    DecodeJson { c =>
-      if (c.focus.string.exists(_ == key.value.name))
-        Json.obj().as(headDecode.value).map(h => Inl(field(h)))
-      else
-        (c --\ key.value.name).focus match {
-          case Some(headJson) => headJson.as(headDecode.value).map(h => Inl(field(h)))
-          case None           => tailDecode.value.decode(c).map(Inr(_))
-        }
-    }
+    implicit def cconsDecodeJson[K <: Symbol, H, T <: Coproduct](implicit
+      key: Witness.Aux[K],
+      headDecode: Lazy[DecodeJson[H]],
+      tailDecode: Lazy[DecodeJson[T]]
+    ): DecodeJson[FieldType[K, H] :+: T] =
+      DecodeJson { c =>
+        if (c.focus.string.exists(_ == key.value.name))
+          Json.obj().as(headDecode.value).map(h => Inl(field(h)))
+        else
+          (c --\ key.value.name).focus match {
+            case Some(headJson) => headJson.as(headDecode.value).map(h => Inl(field(h)))
+            case None           => tailDecode.value.decode(c).map(Inr(_))
+          }
+      }
 
-  implicit def projectDecodeJson[F, G](implicit
-    gen: LabelledGeneric.Aux[F, G],
-    decode: Lazy[DecodeJson[G]]
-  ): DecodeJson[F] =
-    decode.value.map(gen.from)
+    implicit def projectDecodeJson[F, G](implicit
+      gen: LabelledGeneric.Aux[F, G],
+      decode: Lazy[DecodeJson[G]]
+    ): DecodeJson[F] =
+      decode.value.map(gen.from)
 
-  } // DecodeJsonDeriver
+  }
 
 
   implicit def decodeJsonDeriver[T](implicit orphan: Orphan[DecodeJson, DecodeJsonDeriver.type, T]): DecodeJson[T] =
--- src/main/scala/argonaut/AutoEncodeJsons.scala
+++ src/main/scala/argonaut/AutoEncodeJsons.scala
@@ -7,38 +7,38 @@ trait AutoEncodeJsons {
 
   object EncodeJsonDeriver {
 
-  implicit def hnilEncodeJson[L <: HNil]: EncodeJson[L] =
-    EncodeJson(_ => jEmptyObject)
+    implicit def hnilEncodeJson[L <: HNil]: EncodeJson[L] =
+      EncodeJson(_ => jEmptyObject)
 
-  implicit def hconsEncodeJson[K <: Symbol, H, T <: HList](implicit
-    key: Witness.Aux[K],
-    headEncode: Lazy[EncodeJson[H]],
-    tailEncode: Lazy[EncodeJson[T]]
-  ): EncodeJson[FieldType[K, H] :: T] =
-    EncodeJson { case (h :: t) =>
-      (key.value.name -> headEncode.value.encode(h)) ->: tailEncode.value.encode(t)
-    }
+    implicit def hconsEncodeJson[K <: Symbol, H, T <: HList](implicit
+      key: Witness.Aux[K],
+      headEncode: Lazy[EncodeJson[H]],
+      tailEncode: Lazy[EncodeJson[T]]
+    ): EncodeJson[FieldType[K, H] :: T] =
+      EncodeJson { case (h :: t) =>
+        (key.value.name -> headEncode.value.encode(h)) ->: tailEncode.value.encode(t)
+      }
 
-  implicit val cnilEncodeJson: EncodeJson[CNil] =
-    EncodeJson(_ => jEmptyObject)
+    implicit val cnilEncodeJson: EncodeJson[CNil] =
+      EncodeJson(_ => jEmptyObject)
 
-  implicit def cconsEncodeJson[K <: Symbol, H, T <: Coproduct](implicit
-    key: Witness.Aux[K],
-    headEncode: Lazy[EncodeJson[H]],
-    tailEncode: Lazy[EncodeJson[T]]
-  ): EncodeJson[FieldType[K, H] :+: T] =
-    EncodeJson {
-      case Inl(h) => Json(key.value.name -> headEncode.value.encode(h))
-      case Inr(t) => tailEncode.value.encode(t)
-    }
+    implicit def cconsEncodeJson[K <: Symbol, H, T <: Coproduct](implicit
+      key: Witness.Aux[K],
+      headEncode: Lazy[EncodeJson[H]],
+      tailEncode: Lazy[EncodeJson[T]]
+    ): EncodeJson[FieldType[K, H] :+: T] =
+      EncodeJson {
+        case Inl(h) => Json(key.value.name -> headEncode.value.encode(h))
+        case Inr(t) => tailEncode.value.encode(t)
+      }
 
-  implicit def projectEncodeJson[F, G](implicit
-    gen: LabelledGeneric.Aux[F, G],
-    encode: Lazy[EncodeJson[G]]
-  ): EncodeJson[F] =
-    encode.value.contramap(gen.to)
+    implicit def projectEncodeJson[F, G](implicit
+      gen: LabelledGeneric.Aux[F, G],
+      encode: Lazy[EncodeJson[G]]
+    ): EncodeJson[F] =
+      encode.value.contramap(gen.to)
 
-  } // EncodeJsonDeriver
+  }
 
 
   implicit def encodeJsonDeriver[T](implicit orphan: Orphan[EncodeJson, EncodeJsonDeriver.type, T]): EncodeJson[T] =
