aef6932ce222549b01c776c491c2d01231c7924b
sh0hei <vividbit@gmail.com>
2017-09-20 19:47:59 +0900

    update some libraries

--- project/Deps.scala
+++ project/Deps.scala
@@ -2,9 +2,9 @@ import sbt._
 
 object Deps {
   def argonaut = "io.argonaut" %% "argonaut" % "6.2"
-  def refined = "eu.timepit" %% "refined" % "0.6.0"
+  def refined = "eu.timepit" %% "refined" % "0.8.3"
   def shapeless = "com.chuusai" %% "shapeless" % "2.3.2"
 
-  def scalacheckShapeless = "com.github.alexarchambault" %% "scalacheck-shapeless_1.13" % "1.1.3"
-  def utest = "com.lihaoyi" %% "utest" % "0.4.4"
+  def scalacheckShapeless = "com.github.alexarchambault" %% "scalacheck-shapeless_1.13" % "1.1.5"
+  def utest = "com.lihaoyi" %% "utest" % "0.5.3"
 }
