99d99b40aa9a0235f6b9b946e79032ad0c90c910
Alexandre Archambault <alexandre.archambault@gmail.com>
2018-01-01 21:27:38 +0100

    Switch to shapeless 2.3.3

--- project/Deps.scala
+++ project/Deps.scala
@@ -7,12 +7,7 @@ import scala.scalanative.sbtplugin.NativePlatform
 object Deps {
   def argonaut = setting("io.argonaut" %%% "argonaut" % "6.2.1")
   def refined = "eu.timepit" %% "refined" % "0.8.5"
-  def shapeless = setting {
-    if (crossPlatform.value == NativePlatform)
-      "com.github.alexarchambault" %%% "shapeless" % "2.3.3-pre-1"
-    else
-      "com.chuusai" %%% "shapeless" % "2.3.2"
-  }
+  def shapeless = setting("com.chuusai" %%% "shapeless" % "2.3.3")
 
   def scalacheckShapeless = "com.github.alexarchambault" %% "scalacheck-shapeless_1.13" % "1.1.5"
   def utest = "com.lihaoyi" %% "utest" % "0.6.2"
