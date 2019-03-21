399bc82a27a8dddb412e4ac690dce17085ce018e
Alexandre Archambault <alexandre.archambault@gmail.com>
2019-03-21 10:08:41 +0100

    Switch to refined 0.9.4

--- project/Deps.scala
+++ project/Deps.scala
@@ -5,7 +5,7 @@ import org.portablescala.sbtplatformdeps.PlatformDepsPlugin.autoImport._
 
 object Deps {
   def argonaut = setting("io.argonaut" %%% "argonaut" % "6.2.3")
-  def refined = "eu.timepit" %% "refined" % "0.9.2"
+  def refined = "eu.timepit" %% "refined" % "0.9.4"
   def shapeless = setting("com.chuusai" %%% "shapeless" % "2.3.3")
 
   def scalacheckShapeless = setting("com.github.alexarchambault" %%% "scalacheck-shapeless_1.13" % "1.1.8")
