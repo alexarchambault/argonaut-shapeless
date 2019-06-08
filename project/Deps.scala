352d99c21d7d07649d5a8d9781ce76d6e6ee38a1
Alexandre Archambault <alexandre.archambault@gmail.com>
2019-06-08 16:02:26 +0200

    Switch to scala 2.13.0

--- project/Deps.scala
+++ project/Deps.scala
@@ -1,13 +1,20 @@
 import sbt._
 import sbt.Def.setting
+import sbt.Keys.scalaVersion
 
 import org.portablescala.sbtplatformdeps.PlatformDepsPlugin.autoImport._
 
 object Deps {
   def argonaut = setting("io.argonaut" %%% "argonaut" % "6.2.3")
-  def refined = "eu.timepit" %% "refined" % "0.9.5"
+  def refined = "eu.timepit" %% "refined" % "0.9.8"
   def shapeless = setting("com.chuusai" %%% "shapeless" % "2.3.3")
 
-  def scalacheckShapeless = setting("com.github.alexarchambault" %%% "scalacheck-shapeless_1.14" % "1.2.2")
-  def utest = setting("com.lihaoyi" %%% "utest" % "0.6.7")
+  def scalacheckShapeless = setting("com.github.alexarchambault" %%% "scalacheck-shapeless_1.14" % "1.2.3")
+  def utest = setting {
+    val sv = scalaVersion.value
+    val ver =
+      if (sv.startsWith("2.11")) "0.6.7"
+      else "0.6.9"
+    "com.lihaoyi" %%% "utest" % ver
+  }
 }
