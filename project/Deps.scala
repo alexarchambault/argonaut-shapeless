1df5aa4fa08158790da5a91b182428ee3ee90dab
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2019-07-29 09:43:57 +0200

    Update refined to 0.9.9 (#134)

--- project/Deps.scala
+++ project/Deps.scala
@@ -6,7 +6,7 @@ import org.portablescala.sbtplatformdeps.PlatformDepsPlugin.autoImport._
 
 object Deps {
   def argonaut = setting("io.argonaut" %%% "argonaut" % "6.2.3")
-  def refined = "eu.timepit" %% "refined" % "0.9.8"
+  def refined = "eu.timepit" %% "refined" % "0.9.9"
   def shapeless = setting("com.chuusai" %%% "shapeless" % "2.3.3")
 
   def scalacheckShapeless = setting("com.github.alexarchambault" %%% "scalacheck-shapeless_1.14" % "1.2.3")
