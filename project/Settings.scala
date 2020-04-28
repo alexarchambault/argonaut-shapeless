47e129b1818918aa547c6e789e9ffc5eb2ca519e
Alexandre Archambault <alexandre.archambault@gmail.com>
2020-04-28 18:17:46 +0200

    Update scala to 2.13.2

--- project/Settings.scala
+++ project/Settings.scala
@@ -7,7 +7,7 @@ object Settings {
 
   private val scala211 = "2.11.12"
   private val scala212 = "2.12.11"
-  private val scala213 = "2.13.1"
+  private val scala213 = "2.13.2"
 
   lazy val shared = Seq(
     scalaVersion := scala212,
