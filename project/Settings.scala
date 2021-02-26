ca22e58a2ef16b4c85bb6e921267f794cc8108db
Scala Steward <me@scala-steward.org>
2021-02-26 22:10:00 +0100

    Update scala-library to 2.13.5

--- project/Settings.scala
+++ project/Settings.scala
@@ -7,7 +7,7 @@ import Aliases._
 
 object Settings {
 
-  private val scala211 = "2.11.12"
+  private val scala211 = "2.13.5"
   private val scala212 = "2.12.11"
   private val scala213 = "2.13.2"
 
