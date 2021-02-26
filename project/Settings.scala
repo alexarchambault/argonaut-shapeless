e98108d142bfddbee3940e1ae3142909ec7a6740
Scala Steward <me@scala-steward.org>
2021-02-26 22:10:05 +0100

    Update scala-library to 2.12.13

--- project/Settings.scala
+++ project/Settings.scala
@@ -8,7 +8,7 @@ import Aliases._
 object Settings {
 
   private val scala211 = "2.11.12"
-  private val scala212 = "2.12.11"
+  private val scala212 = "2.12.13"
   private val scala213 = "2.13.2"
 
   lazy val shared = Seq(
