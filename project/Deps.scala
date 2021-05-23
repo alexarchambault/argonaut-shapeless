08cfa7f3aaf8206fe3c98c83a9ff852c18cedf34
Alexandre Archambault <alexandre.archambault@gmail.com>
2021-05-23 14:41:05 +0200

    Update scalacheck-shapeless to 1.3.0

--- project/Deps.scala
+++ project/Deps.scala
@@ -9,6 +9,6 @@ object Deps {
   def refined = "eu.timepit" %% "refined" % "0.9.12"
   def shapeless = setting("com.chuusai" %%% "shapeless" % "2.3.7")
 
-  def scalacheckShapeless = setting("com.github.alexarchambault" %%% "scalacheck-shapeless_1.14" % "1.2.5")
+  def scalacheckShapeless = setting("com.github.alexarchambault" %%% "scalacheck-shapeless_1.15" % "1.3.0")
   def utest = setting("com.lihaoyi" %%% "utest" % "0.7.10")
 }
