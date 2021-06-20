d31b06c61dc7ddb429b31178b32741ed469d5e60
Scala Steward <me@scala-steward.org>
2021-06-20 16:02:57 +0200

    Update argonaut to 6.3.4

--- project/Deps.scala
+++ project/Deps.scala
@@ -5,7 +5,7 @@ import sbt.Keys.scalaVersion
 import org.portablescala.sbtplatformdeps.PlatformDepsPlugin.autoImport._
 
 object Deps {
-  def argonaut = setting("io.argonaut" %%% "argonaut" % "6.3.3")
+  def argonaut = setting("io.argonaut" %%% "argonaut" % "6.3.4")
   def refined = setting("eu.timepit" %%% "refined" % "0.9.25")
   def shapeless = setting("com.chuusai" %%% "shapeless" % "2.3.7")
 
