db5d973655316fcfe5225e6c7c4fe79c6876898f
Scala Steward <me@scala-steward.org>
2022-03-27 22:04:27 +0200

    Update shapeless to 2.3.9

--- project/Deps.scala
+++ project/Deps.scala
@@ -7,7 +7,7 @@ import org.portablescala.sbtplatformdeps.PlatformDepsPlugin.autoImport._
 object Deps {
   def argonaut = setting("io.argonaut" %%% "argonaut" % "6.3.7")
   def refined = setting("eu.timepit" %%% "refined" % "0.9.28")
-  def shapeless = setting("com.chuusai" %%% "shapeless" % "2.3.7")
+  def shapeless = setting("com.chuusai" %%% "shapeless" % "2.3.9")
 
   def scalacheckShapeless = setting("com.github.alexarchambault" %%% "scalacheck-shapeless_1.15" % "1.3.0")
   def utest = setting("com.lihaoyi" %%% "utest" % "0.7.10")
